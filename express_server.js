2ab39326d35ccd3b35be0cc668fa8788fc775764
sogbdn <sonia.barretdenazaris@gmail.com>
2019-01-19 20:05:37 +0000

    Commit with Login and register on 1st page

--- express_server.js
+++ express_server.js
@@ -3,21 +3,20 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const cookieParser = require('cookie-parser');
 
+// express setup
 const PORT = 8080; //___________ default port 8080
 const app = express(); //_________My App is running with Express
 
-//_______Use body Parser to access POST request parameters
-//_______ parsed entire body to JS object
+//______Middleware: body Parser to access POST request parameters
+//_______ (parsed entire body to JS object)
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cookieParser());
-
 //___________ Tells Express app to use EJS as its templating engine
 app.set("view engine", "ejs");
 
-//________Use UUID to generate random string____________________________//
-const uuidv1 = require("uuid/v1");
 
-//_________MY DATABASE OBJECT = urlDatabase _____________________________//
+//_____________________DATABASE_____________________________________//
+//_________DATABASE OBJECT = urlDatabase _____________________________//
 var urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com",
@@ -37,44 +36,51 @@ const users = {
   }
 };
 
-// function that produces a string of 6 random alphanumeric characters:
-function generateRandomString() {
-   return uuidv1().substr(0, 6);
-}
+// Create a new user object and add to the users db
+// Return the new user object
+// const newUser = {
+//   id: userId,
+//   email: email,
+//   password: password
+// };
+// users[newUser.id] = newUser;
+// console.log(users)
 
+//________Use UUID to generate random string____________________________//
+const uuidv1 = require("uuid/v1");
+
+//_____ function that produces a string of 6 random alphanumeric characters:
+function generateRandomString() {
+  return uuidv1().substr(0, 6);
+}
+//______Create a new user and add it to the users db and return the userId
 const createUser = (email, password) => {
   // generate a random id
   const userId = generateRandomString();
-
- 
-  // Create a new user object
-  // add the new user object to the users db
-  // Return the new user object
+  // create a new user object
   const newUser = {
     id: userId,
     email: email,
     password: password,
-  };
-  users[userId] = newUser;
-  return userId;
-};
- // Maybe later on I should I might change this function to check password as well
+  }
+}
+// add the new user object to the users db
+// users[userId] = newUser;
+
+ // Maybe later on I should change this function to check password as well
 const findUser = email => {
-  // loop the existing user objects
-  // Then return the userId
+  // loop through the existing user objects
   for (const userId in users) {
+  // if the email is the same then I got the right user
+  // Then return the userId
     if (users[userId].email === email) {
       return userId;
     }
   }
   return false;
-  // if the email is the same then I got the right user
-  // Then assign its user id to userId
+}; 
  
-}
-
-// app.get to add a new route handler
-// this lines registers a handler on the root path
+// app.get to add a new route handler on the root path
 app.get("/", (req, res) => {
   res.send("Hello!");
 });
@@ -88,20 +94,20 @@ app.get("/hello", (req, res) => {
 // Route order: most specific to less specific
 
 app.get("/urls", (req, res) => {
-  let templateVars = { urls: urlDatabase, user: req.cookies.user_id};
-
+  let templateVars = { urls: urlDatabase, user: users[req.cookies.user_id]};
+  res.render('urls_index', templateVars);
   //passing username to each EJS template so that it knows if the user logged in and username
   res.render("urls_index", templateVars);
 });
 
-// Add GET route to show the form in the wb page
+// Add GET route to show the form in the web page
 app.get("/urls/new", (req, res) => {
   res.render('urls_new'); 
 });
 
-// Register endpoints
-
+//_______________ REGISTER endpoints
 // Display the register form
+
 app.get('/register', (req, res) => {
   res.cookie('user id', userId);
   res.redirect('/urls');  
@@ -110,25 +116,27 @@ app.get('/register', (req, res) => {
 
 // Create a new user
 app.post('/register', (req, res) => {
-  // Extract the info from th form information; email and passport
+  // Extract the info from form information; email and passport
   const email = req.body.email;
   const password = req.body.password;
+
+  // If the email or password are empty strings
   const email_password_empty = !email || !password;
 
-// If the email or password are empty strings
-// send back a response with the 400 status
+  // send back a response with the 400 status
   if (email_password_empty) {
-    res.status(400).send('Please fill out all the fields');
+    res.status(400).send('Please fill out all the required fields');
+  // If someone tries to register with an existing user's email
   } else if (emailExists(email)) { 
-  res.status(400).send('That email already exists. Please Login');
+    res.status(400).send('That email already exists. Please Login');
   } else {
   // create a new user and add it to the global users
   const userId = createUser(email, password); 
   // set a cookie with the userId
-  res.cookie('userId, userId');
+  res.cookie('user_id', userId);
   // redirect to '/urls'
   res.redirect('/urls');
-  };
+  }
 });
 
 // Endpoint Edit URL form
@@ -147,7 +155,7 @@ app.post("/urls", (req, res) => {
   let rNumber = generateRandomString();
   urlDatabase[rNumber] = req.body.longURL;
   console.log(urlDatabase);
-  res.redirect('/urls'); // redirect to home home page
+  res.redirect('/urls'); // redirect client to home home page
 });
 
 // Add a POST route that removes a URL resource from request: /urls/:id/delete
@@ -177,4 +185,3 @@ app.post("/logout", (req, res) => {
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
 });
-
